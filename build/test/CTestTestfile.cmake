1409f764f005b23fba44bea2a7ed5ff10fd81768
Vitaliy Sobol <vitaliy@2solar.nl>
2024-04-05 16:27:26 +0300

    add script

---
+++ build/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/vitaliysobol/projects/personal/cpp/json-parser/test
+# Build directory: /Users/vitaliysobol/projects/personal/cpp/json-parser/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[ExampleTests]=] "/Users/vitaliysobol/projects/personal/cpp/json-parser/build/test/ExampleTests")
+set_tests_properties([=[ExampleTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/vitaliysobol/projects/personal/cpp/json-parser/test/CMakeLists.txt;15;add_test;/Users/vitaliysobol/projects/personal/cpp/json-parser/test/CMakeLists.txt;0;")
---
+++ build_and_test.lua
@@ -0,0 +1,33 @@
+-- Lua script to automate the setup, build, and test process
+
+-- Function to execute shell commands
+function execute_command(command)
+    local handle = io.popen(command)
+    local result = handle:read("*a")
+    handle:close()
+    return result
+end
+
+-- Clone Google Test repository
+local git_clone_command = "git clone https://github.com/google/googletest.git"
+print("Cloning Google Test repository...")
+execute_command(git_clone_command)
+
+-- Set up the project with CMake
+local cmake_build_directory = "build"
+local cmake_command = "cmake -S . -B " .. cmake_build_directory
+print("Setting up project with CMake...")
+execute_command(cmake_command)
+
+-- Build the project
+local make_command = "cmake --build " .. cmake_build_directory
+print("Building the project...")
+execute_command(make_command)
+
+-- Run the tests
+local test_command = "cd " .. cmake_build_directory .. " && ctest"
+print("Running tests...")
+execute_command(test_command)
+
+print("Done.")
+
