2d876cc03aa3aba68fcffed0b8d69400b3fa84d1
Vitaliy Sobol <vitaliy@2solar.nl>
2024-06-04 17:51:47 +0300

    cleaning project

--- build-vscode/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/vitaliysobol/projects/personal/cpp/json-parser
-# Build directory: /Users/vitaliysobol/projects/personal/cpp/json-parser/build-vscode
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[ExampleTests]=] "/Users/vitaliysobol/projects/personal/cpp/json-parser/build-vscode/test/ExampleTests")
-set_tests_properties([=[ExampleTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/vitaliysobol/projects/personal/cpp/json-parser/CMakeLists.txt;32;add_test;/Users/vitaliysobol/projects/personal/cpp/json-parser/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
